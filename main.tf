db02885965550985fa16eee6af6c5b15fb47d221
Chris Diehl <cultclassik@gmail.com>
2021-07-12 07:16:40 -0500

    add ansible control

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "github" {
-  token = var.github_token
+  #token = var.github_token
 }
 
 data "terraform_remote_state" "tfcloud" {
--- modules/github_repo/main.tf
+++ modules/github_repo/main.tf
@@ -1,5 +1,5 @@
 resource "github_repository" "repo" {
   name        = var.repo_name
-  description = var.repo_desc
+  description = "${var.repo_desc} - Repo managed by Terraform"
   visibility  = var.visibility
 }
