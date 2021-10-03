5ddd1e9e12045ccd6841f530dc1ea4ccf37ee9b7
Chris Diehl <cultclassik@gmail.com>
2021-10-03 18:46:38 -0500

    add repo

--- repos.tf
+++ repos.tf
@@ -4,8 +4,14 @@ module "azdo_terraform_pipeline_templates" {
   repo_desc = "Azure Pipelines template repo for Terraform pipelines"
 }
 
-module "azdo_terraform_pipeline_templates" {
+module "terraform-azurerm-o365dns" {
   source    = "./modules/github_repo"
   repo_name = "terraform-azurerm-o365dns"
+  repo_desc = "Terraform module for creating DNS records in Azure DNS for Office 365"
+}
+
+module "terraform-azurerm-github-repos" {
+  source    = "./modules/github_repo"
+  repo_name = "erraform-azurerm-github-repos"
   repo_desc = "Terraform module for creating DNS records in Azure DNS for Office 365"
 }
