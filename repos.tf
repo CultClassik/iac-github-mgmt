17fd1fab2b69542e2bf1df9f498f88dedf657034
Chris Diehl <cultclassik@gmail.com>
2021-10-03 18:44:07 -0500

    add tf mod repo for o365 dns

--- repos.tf
+++ repos.tf
@@ -3,3 +3,9 @@ module "azdo_terraform_pipeline_templates" {
   repo_name = "azdo-terraform-pipeline-templates"
   repo_desc = "Azure Pipelines template repo for Terraform pipelines"
 }
+
+module "azdo_terraform_pipeline_templates" {
+  source    = "./modules/github_repo"
+  repo_name = "terraform-azurerm-o365dns"
+  repo_desc = "Terraform module for creating DNS records in Azure DNS for Office 365"
+}
