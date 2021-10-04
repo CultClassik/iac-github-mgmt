3c2daf5ddd2f8a78853eaeb1775badf315c24a43
Chris Diehl <cultclassik@gmail.com>
2021-10-04 14:13:04 -0500

    add tkg repo

--- repos.tf
+++ repos.tf
@@ -4,16 +4,7 @@ module "azdo_terraform_pipeline_templates" {
   repo_desc = "Azure Pipelines template repo for Terraform pipelines"
 }
 
-locals {
-  repos = {
-    tf_module = {
-      terraform-module-scaffolding   = "Base repo for a new Terraform module"
-      terraform-azurerm-o365dns      = "Terraform module for creating DNS records in Azure DNS for Office 365"
-      terraform-azurerm-github-repos = "Terraform module for creating Github repos"
-    }
-  }
-}
-
+# TF Modules
 resource "github_actions_secret" "tf_api_token" {
   for_each        = local.repos.tf_module
   repository      = each.key
@@ -23,6 +14,14 @@ resource "github_actions_secret" "tf_api_token" {
 
 module "terraform_module_repos" {
   for_each  = local.repos.tf_module
+  source    = "./modules/github_repo"
+  repo_name = each.key
+  repo_desc = each.value
+}
+
+# Misc projects
+module "misc_project_repos" {
+  for_each  = local.repos.misc_projects
   source    = "./modules/github_repo"
   repo_name = each.key
   repo_desc = each.value
