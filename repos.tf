1f37b6ca20d34b1a891047e7dcae2b5427769542
Chris Diehl <cultclassik@gmail.com>
2021-10-03 19:14:02 -0500

    add tf token and use each loops

--- repos.tf
+++ repos.tf
@@ -4,20 +4,26 @@ module "azdo_terraform_pipeline_templates" {
   repo_desc = "Azure Pipelines template repo for Terraform pipelines"
 }
 
-module "terraform-module-scaffolding" {
-  source    = "./modules/github_repo"
-  repo_name = "terraform-module-scaffolding"
-  repo_desc = "Base repo for a new Terraform module"
+locals {
+  repos = {
+    tf_module = {
+      terraform-module-scaffolding   = "Base repo for a new Terraform module"
+      terraform-azurerm-o365dns      = "Terraform module for creating DNS records in Azure DNS for Office 365"
+      terraform-azurerm-github-repos = "Terraform module for creating Github repos"
+    }
+  }
 }
 
-module "terraform-azurerm-o365dns" {
-  source    = "./modules/github_repo"
-  repo_name = "terraform-azurerm-o365dns"
-  repo_desc = "Terraform module for creating DNS records in Azure DNS for Office 365"
+resource "github_actions_secret" "tf_api_token" {
+  for_each        = locals.repos.tf_module
+  repository      = each.key
+  secret_name     = "TF_API_TOKEN"
+  plaintext_value = var.tfe_token
 }
 
-module "terraform-azurerm-github-repos" {
+module "terraform_module_repos" {
+  for_each  = local.repos.tf_module
   source    = "./modules/github_repo"
-  repo_name = "terraform-azurerm-github-repos"
-  repo_desc = "Terraform module for creating Github repos"
+  repo_name = each.key
+  repo_desc = each.value
 }
--- variables.tf
+++ variables.tf
@@ -1,5 +1,12 @@
 variable "github_token" {
   description = "The Github PAT"
   type        = string
+  sensitive   = true
   default     = null
+}
+
+variable "tfe_token" {
+  description = "The TFCloud API Token"
+  type        = string
+  sensitive   = true
 }
