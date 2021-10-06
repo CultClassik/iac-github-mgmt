fed241c4b5401cdbe8eca346903f1a4257785217
Chris Diehl <cultclassik@gmail.com>
2021-10-06 08:55:37 -0500

    add k8s node ansible role

--- locals.tf
+++ locals.tf
@@ -18,7 +18,7 @@ locals {
       ansible-role-docker           = "Ansible role for managing Docker"
       ansible-role-kubetools        = "Ansible role for managing Kubernetes client tooling"
       ansible-role-k8s-certificates = "Ansible role for managing k8s-certificates"
-      #ansible-role-k8s-node = "Ansible role for managing Kubernetes nodes"
+      ansible-role-k8s-node         = "Ansible role for managing Kubernetes nodes"
     }
     misc_projects = {
     }
