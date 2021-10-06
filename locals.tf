30d71d9711b3c471ad2a93283760c659bb3dfffb
Chris Diehl <cultclassik@gmail.com>
2021-10-05 19:17:24 -0500

    update workspace

--- locals.tf
+++ locals.tf
@@ -4,6 +4,7 @@ locals {
       terraform-module-scaffolding   = "Base repo for a new Terraform module"
       terraform-azurerm-o365dns      = "Terraform module for creating DNS records in Azure DNS for Office 365"
       terraform-azurerm-github-repos = "Terraform module for creating Github repos"
+      iac-aws-resources              = "Terraform code to manage AWS resources"
     }
     iac = {
       #iac-github-mgmt = "Manages Github resources"
