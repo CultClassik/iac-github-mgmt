a43a081f1aab3d6c262abffd7cbcd0a25abe2d3a
Chris Diehl <cultclassik@gmail.com>
2021-10-10 22:22:20 -0500

    add ansible-role-git

--- repos_iac.tf
+++ repos_iac.tf
@@ -1,6 +1,6 @@
 # iac repos - personal
 module "iac_repos" {
-  for_each  = local.repos.iac
+  for_each  = local.cultclassik.repos.iac
   source    = "./modules/github_repo"
   repo_name = each.key
   repo_desc = each.value.desc
@@ -19,7 +19,7 @@ module "diehlabs_iac_repos" {
 
 # iac repo secrets
 resource "github_actions_secret" "tf_api_token" {
-  for_each        = merge(local.repos.tf_module, local.repos.iac)
+  for_each        = merge(local.cultclassik.repos.tf_module, local.cultclassik.repos.iac)
   repository      = each.key
   secret_name     = "TF_API_TOKEN"
   plaintext_value = var.tfe_token
@@ -27,7 +27,7 @@ resource "github_actions_secret" "tf_api_token" {
 
 # terraform cloud workspaces
 resource "tfe_workspace" "iac_ws" {
-  for_each       = local.repos.iac
+  for_each       = local.cultclassik.repos.iac
   name           = each.key
   description    = "${each.value.desc} - Managed by Terraform"
   organization   = "Diehlabs"
--- repos_terraform_module.tf
+++ repos_terraform_module.tf
@@ -1,6 +1,6 @@
 # TF Modules
 module "terraform_module_repos" {
-  for_each  = local.repos.tf_module
+  for_each  = local.cultclassik.repos.tf_module
   source    = "./modules/github_repo"
   repo_name = each.key
   repo_desc = each.value
