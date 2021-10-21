b71c81e75ff89723571dbecef3cbcd253a902f80
Chris Diehl <cultclassik@gmail.com>
2021-10-21 04:48:07 -0500

    move ansible-role-docker to diehlabs

--- locals.tf
+++ locals.tf
@@ -12,6 +12,7 @@ locals {
         ansible-role-git       = "Ansible role for managing Git client tooling"
         ansible-role-terraform = "Ansible role for managing Terraform and related tooling"
         ansible-role-kubetools = "Ansible role for managing Kubernetes client tooling"
+        ansible-role-docker    = "Ansible role for managing Docker"
       }
     }
   }
@@ -33,7 +34,6 @@ locals {
         ansible-role-vagrant          = "Ansible role for managing Vagrant"
         ansible-role-zsh              = "Ansible role for managing zsh"
         ansible-role-cryptominer      = "Ansible role for managing cryptominer"
-        ansible-role-docker           = "Ansible role for managing Docker"
         ansible-role-k8s-certificates = "Ansible role for managing k8s-certificates"
         ansible-role-k8s-node         = "Ansible role for managing Kubernetes nodes"
       }
