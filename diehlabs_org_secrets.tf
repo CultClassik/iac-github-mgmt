41bd7f1918a4ccf1860ad1700da636ee1764057a
Chris Diehl <cultclassik@gmail.com>
2022-02-28 15:01:44 -0600

    change private to all for tf module test builds

--- diehlabs_org_secrets.tf
+++ diehlabs_org_secrets.tf
@@ -4,28 +4,28 @@ data "vault_generic_secret" "spn_terraform" {
 
 resource "github_actions_organization_secret" "gh_arm_client_id" {
   provider        = github.diehlabs
-  visibility      = "private"
+  visibility      = "all"
   secret_name     = "ARM_CLIENT_ID"
   plaintext_value = data.vault_generic_secret.spn_terraform.data["ARM_CLIENT_ID"]
 }
 
 resource "github_actions_organization_secret" "gh_arm_client_secret" {
   provider        = github.diehlabs
-  visibility      = "private"
+  visibility      = "all"
   secret_name     = "ARM_CLIENT_SECRET"
   plaintext_value = data.vault_generic_secret.spn_terraform.data["ARM_CLIENT_SECRET"]
 }
 
 resource "github_actions_organization_secret" "gh_arm_tenant_id" {
   provider        = github.diehlabs
-  visibility      = "private"
+  visibility      = "all"
   secret_name     = "ARM_TENANT_ID"
   plaintext_value = data.vault_generic_secret.spn_terraform.data["ARM_TENANT_ID"]
 }
 
 resource "github_actions_organization_secret" "gh_arm_sub_id" {
   provider        = github.diehlabs
-  visibility      = "private"
+  visibility      = "all"
   secret_name     = "ARM_SUBSCRIPTION_ID"
   plaintext_value = data.vault_generic_secret.spn_terraform.data["ARM_SUBSCRIPTION_ID"]
 }
